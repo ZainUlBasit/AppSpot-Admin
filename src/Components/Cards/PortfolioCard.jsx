f3903e64e7f26a99dfe7d40d92c1482385e622f6
ZainUlBasit <zainulbasit486@gmail.com>
2024-08-08 14:45:42 +0500

    changes

--- src/Components/Cards/PortfolioCard.jsx
+++ src/Components/Cards/PortfolioCard.jsx
@@ -1,28 +1,19 @@
 import React from "react";
 import { MdOutlineArrowOutward } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const PortfolioCard = ({ portfolio }) => {
+  const navigate = useNavigate();
   return (
-    <div className="w-[100%] lg:w-[48%] md:[40%] xl:[48%] sm:w-[48%] h-96 relative ">
-      <img
-        src={portfolio.attachment}
-        alt=""
-        className="w-full h-full object-cover rounded-xl"
-      />
-      <div className="absolute bottom-2 flex justify-between w-full items-center px-4">
-        {/* <div className="w-12 h-12 bg-[#262626] rounded-full flex justify-center items-center">
-          <img src={cardLogo} alt="" className="w-1/2 h-1/2 " />
-        </div> */}
-        <div
-          // to="/portfolio/casestudy"
-          className="bg-[#262626] flex px-3 py-1 text-[#FC9C6E] rounded-lg justify-center items-center gap-1"
-        >
-          <button className="text-xs lg:text-lg font-semibold font-spartan sm:text-xs">
-            View Case Study
-          </button>
-          <span>
-            <MdOutlineArrowOutward />
-          </span>
+    <div className="min-w-[200px] max-w-[400px] relative border-2 border-main overflow-hidden bg-[#716242] rounded-t-[20px]">
+      <img src="/test.png" className="w-full" />
+      <div className="w-full bg-[#483E28] text-white px-3 flex flex-col gap-y-4 py-4 rounded-t-[20px]">
+        <div className="font-semibold text-xl">We helped boost sales</div>
+        <div className="text-[1rem] font-thin">
+          We digitised Costa Coffee in Bahrain and increased their sales.
+        </div>
+        <div className="flex gap-x-2 py-2 items-center justify-end">
+          <img src="/Google.png" alt="nothing" className="w-[150px]" />
+          <img src="/App.png" alt="nothing" className="w-[150px]" />
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        main: "#FF916F",
+      },
+    },
   },
   plugins: [],
-}
+};
