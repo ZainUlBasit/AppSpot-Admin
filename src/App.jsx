9aff3d2c6676df3de880859a387892a1605998f8
ZainUlBasit <zainulbasit486@gmail.com>
2024-08-08 16:36:01 +0500

    channges

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           Add New Portfolio
         </div>
       </div>
-      <div className="flex gap-x-2 gap-y-2 flex-wrap px-2 justify-around items-center">
+      <div className="flex gap-x-2 gap-y-2 flex-wrap px-4 justify-around  items-stretch">
         <PortfolioCard
           mainBgColor={" bg-[#716242]"}
           primaryBgColor={" bg-[#483E28]"}
